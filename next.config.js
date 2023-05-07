e0492721662506057569a44d1bb089c2d4170671
ian.markowitz <ianhmarkowitz@gmail.com>
2023-05-06 19:10:19 -0700

    fix repo name?

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 
+const repo = 'medafford.github.io'
+
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/repository' : '',
-  assetPrefix: isProd ? '/repository/' : ''
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : ''
 }
 
 module.exports = nextConfig
