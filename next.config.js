0dcbd4a1cbb435c6d3a77c7f89b4c42e8561f281
ian.markowitz <ianhmarkowitz@gmail.com>
2023-05-06 18:53:45 -0700

    fix `,`, add basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: '/repository',
+  assetPrefix: '/repository/'
 }
 
 module.exports = nextConfig
