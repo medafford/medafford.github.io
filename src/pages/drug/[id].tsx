e863360fa2f94ca1bdb272d4ed0b2a5d80ae1267
ian.markowitz <ianhmarkowitz@gmail.com>
2023-05-06 19:58:39 -0700

    update css, add icons

--- src/pages/drug/[id].tsx
+++ src/pages/drug/[id].tsx
@@ -1,6 +1,7 @@
-import {useRouter} from "next/router";
+import styles from '../../page.module.css'
 import {getData, Row} from '../../data'
 import {GetStaticPropsContext} from "next";
+import {Grid} from "@material-ui/core";
 
 export async function getStaticProps(ctx: GetStaticPropsContext) {
   const id = ctx.params?.id as string;
@@ -28,37 +29,48 @@ export async function getStaticPaths() {
   }
 }
 
-export default function Id({ drug }: { drug: Row }) {
+export default function Id({drug}: { drug: Row }) {
   return (
       <main>
-        <h1>{drug.drug}</h1>
+        <h1>{drug.drug}{(drug.generic ? <span>{", "}<i>{drug.generic}</i></span> : "")}</h1>
+        <div className={styles.info}>
         {drug.manufacturer ?
             <p>Manufacturer: {drug.manufacturer}</p>
-            : null}
-        {drug.generic ?
-            <p>Generic: {drug.generic}</p>
             : null}
         {drug.indication ?
             <p>Indication: {drug.indication}</p>
             : null}
-        {drug.pap_insurance ?
-            <p>PAP (insurance): {drug.pap_insurance}</p>
-            : null}
-        {drug.pap_no_insurance ?
-            <p>PAP (no insurance): {drug.pap_no_insurance}</p>
-            : null}
-        {drug.goodrx ?
-            <p>Goodrx: {drug.goodrx}</p>
-            : null}
-        {drug.costplus_drugs ?
-            <p>Costplus_drugs: {drug.costplus_drugs}</p>
-            : null}
-        {drug.copay_cards ?
-            <p>Copay_cards: {drug.copay_cards}</p>
-            : null}
         {drug.notes ?
             <p>Notes: {drug.notes}</p>
             : null}
+        <Grid container spacing={2}>
+          <Grid xs={6}>
+            {drug.pap_no_insurance ?
+                <p>PAP (no insurance): {drug.pap_no_insurance}</p>
+                : null}
+          </Grid>
+          <Grid xs={6}>
+            {drug.goodrx ?
+                <p>Goodrx: {drug.goodrx}</p>
+                : null}
+          </Grid>
+          <Grid xs={6}>
+            {drug.copay_cards ?
+                <p>Copay_cards: {drug.copay_cards}</p>
+                : null}
+          </Grid>
+          <Grid xs={6}>
+            {drug.costplus_drugs ?
+                <p>Costplus_drugs: {drug.costplus_drugs}</p>
+                : null}
+          </Grid>
+          <Grid xs={6}>
+            {drug.pap_insurance ?
+                <p>PAP (insurance): {drug.pap_insurance}</p>
+                : null}
+          </Grid>
+        </Grid>
+        </div>
       </main>
   )
 }
