47276c908359daac00a7394a4c2c42c2d8e009dc
ian.markowitz <ianhmarkowitz@gmail.com>
2023-05-06 19:12:51 -0700

    remove repo name

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 
-const repo = 'medafford.github.io'
-
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : ''
+  basePath: '',
+  assetPrefix: ''
 }
 
 module.exports = nextConfig
