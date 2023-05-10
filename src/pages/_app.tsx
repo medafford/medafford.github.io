25dcc384a9b74c37531209975e51c47a921a484c
ian.markowitz <ianhmarkowitz@gmail.com>
2023-05-10 01:12:56 -0700

    update css order

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {Box, createTheme, ThemeProvider} from '@mui/material';
+import {Box, createTheme, StyledEngineProvider, ThemeProvider} from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import {AppProps} from "next/app";
 import Head from 'next/head';
@@ -25,9 +25,11 @@ export default function App({Component, pageProps}: AppProps<any>) {
           <meta name="viewport" content="initial-scale=1, width=device-width" />
         </Head>
         <CssBaseline />
-        <Box className={styles.main}>
-          <Component {...pageProps} />
-        </Box>
+        <StyledEngineProvider injectFirst>
+          <Box className={styles.main}>
+            <Component {...pageProps} />
+          </Box>
+        </StyledEngineProvider>
       </ThemeProvider>
   )
 }
